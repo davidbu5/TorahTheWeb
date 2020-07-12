0542bae83a97bce18370fc39cc416f1c97cbf9dc
David Bulka <davidbulka@gmail.com>
2020-07-12 19:56:54 +0300

    no need for updates popup

--- src/popups/updateAvailable/updateAvailable.ts
+++
@@ -1,9 +0,0 @@
-window.onload = function () { return sendPopupOpenStateEvent(true); };
-window.onunload = function () { return sendPopupOpenStateEvent(false); };
-function sendPopupOpenStateEvent(isOpen) {
-    var extension = chrome.extension.getBackgroundPage();
-    extension.dispatchEvent(new CustomEvent("popupOpenState", { detail: { isOpen: isOpen } }));
-}
-const downloadExtentionLinkTag = document.getElementById('downloadExtensionLink');
-var SERVER_PATH="localhost";
-downloadExtentionLinkTag.href = "https://" + SERVER_PATH + "/ChromeExtension/extension.crx";
