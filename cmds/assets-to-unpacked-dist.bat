8296f7fb134e9ce1455383aa19d03576c3588fde
David Bulka <davidbulka@gmail.com>
2020-07-12 20:31:46 +0300

    images to static

--- cmds/assets-to-unpacked-dist.bat
+++ cmds/assets-to-unpacked-dist.bat
@@ -1,4 +1,4 @@
 mkdir "unpacked-dist"
 XCOPY manifest.json "unpacked-dist" /i/y 
 XCOPY src\\popups "unpacked-dist" /s/i/y/exclude:cmds\\excludedfileslist.txt
-XCOPY images "unpacked-dist" /s/i/y
+XCOPY static\\images "unpacked-dist" /s/i/y
